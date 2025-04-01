b967c9adb16269639a59e5f752c11a27beeb21a0
suvobd100 <makbarbd@gmail.com>
2025-04-02 02:13:52 +0600

    added single food route by food id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 8000;
@@ -32,6 +32,15 @@ async function run() {
       const result = await foodsCollection.find().toArray();
       res.send(result);
     });
+
+    // get a single food data by _id from db
+    app.get('/food/:id', async (req, res) => {
+        const id = req.params.id
+        console.log(id);
+        const query = { _id: new ObjectId(id) }
+        const result = await foodsCollection.findOne(query)
+        res.send(result)
+      })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
